a873e110bbd8fb95c24928945e49c4562390d55d
Tejas-0570 <waydandetejas111@gmail.com>
2026-03-17 01:59:45 +0530

    Improved version of Peterson Number

--- Numbers/PetersonNumber.java
+++ Numbers/PetersonNumber.java
@@ -37,3 +37,61 @@ class PetersonNumber{
         }
     }
 }
+
+/*
+
+// More Optimized version by using Array
+
+import java.util.Scanner;
+class PetersonNumber{
+    // Pre-compute factorials for digits 0-9 and store in an array
+    static int[] preComputeFactorial(){
+        int[] fact = new int[10];
+        fact[0] = 1;
+        for(int i = 1; i <= 9; i++){
+            fact[i] = fact[i - 1] * i;
+        }
+        return fact;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        if(!sc.hasNextInt()){
+            System.out.print("Invalid input! Please enter valid input");
+            return;
+        }
+        int num = sc.nextInt();
+
+        if(num < 0){
+            System.out.print("Invalid input! Please enter positive number");
+            return;
+        }
+        if(num == 0){
+            System.out.print("Invalid input! 0 can't be Peterson number");
+            return;
+        }
+
+        int [] fact = preComputeFactorial();
+        int factSum = 0;
+        int temp = num;
+
+        while(temp > 0){
+            int ld = temp % 10;
+            factSum = factSum + fact[ld];
+            temp /= 10;
+        }
+
+        if(num == factSum){
+            System.out.print(num+" is a Peterson Number");
+        }
+        else{
+            System.out.print(num+" is Not a Peterson Number");
+        }
+    }
+}
+
+*/
+
+
+
