2a243874e732acab62a8ac82e0888093ecee2c6e
Tejas-0570 <waydandetejas111@gmail.com>
2026-03-19 00:20:03 +0530

    Disarium Number

--- Numbers/DisariumNumber.java
+++ Numbers/DisariumNumber.java
@@ -1,39 +1,39 @@
 import java.util.Scanner;
-class ArmstrongNumber{
+class DisariumNumber{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number : ");
         int num = sc.nextInt();
         int count = 0;
-
         for(int i = num; i != 0; i /= 10){
             count++;
         }
         int sum = 0;
         for(int i = num; i != 0; i /= 10){
-            int lastDigit = i % 10;
+            int ld = i % 10;
             int pow = 1;
-            for(int j=1; j <= count; j++){
-                pow *= lastDigit;
+            for(int j=1; j<= count; j++){
+                pow *= ld;
             }
             sum += pow;
+            count--;
         }
 
         if(num == sum){
-            System.out.print(num+" is a Armstrong Number");
+            System.out.print(num+" is a Disarium Number");
         } else {
-            System.out.print(num+" is Not a Armstron Number");
+            System.out.print(num+" is Not a Disarium Number");
         }
     }
 }
 
-// ----------------------------- Clean Code ---------------------------------------------------
 
+//----------------------------------------- Clean Code ----------------------------------------------------
 /*
 import java.util.Scanner;
-class ArmStrongNumber{
+class DisariumNumber{
 
-    static int DigitCount(int n){
+    static int digitCount(int n){
         int count = 0;
         while(n > 0){
             count++;
@@ -58,23 +58,28 @@ class ArmStrongNumber{
             return;
         }
         int num = sc.nextInt();
-        if(num < 0){
+        if(num < 0) {
             System.out.print("Invalid input! Please enter positive number");
             return;
         }
-        int count = DigitCount(num);
-        int temp = num, sum = 0;
+        if(num == 0){
+            System.out.print(num+"is Not a Disarium Number");
+            return;
+        }
 
-        while(temp > 0){
-            int lastDigi = temp % 10;
-            sum += power(lastDigi, count);
-            temp /= 10;
+        int count = digitCount(num);
+        int sum = 0;
+
+        for(int i = num; i != 0; i /= 10){
+            int lastDigit = i % 10;
+            sum += power(lastDigit, count);
+            count--;
         }
 
         if(num == sum){
-            System.out.print(num+" is an Armstrong Number");
+            System.out.print(num+" is a Disarium Number");
         } else {
-            System.out.print(num+" is Not an Armstrong Number");
+            System.out.print(num+" is not a Disarium Number");
         }
     }
 }
