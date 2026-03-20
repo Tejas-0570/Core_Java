3d360dc4f531195a54dc54b56f80c0944598b538
Tejas-0570 <waydandetejas111@gmail.com>
2026-03-21 01:30:43 +0530

    LargestOfThreeNumber

---
+++ Numbers/LargestOfThreeNumber.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class LargestOfThreeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number : ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number : ");
+        int num2 = sc.nextInt();
+        System.out.print("Enter third number : ");
+        int num3 = sc.nextInt();
+
+        int result = num1>num2 && num1>num3? num1 : (num2 > num3 ? num2 : num3);
+
+        System.out.print(result);
+    }
+}
