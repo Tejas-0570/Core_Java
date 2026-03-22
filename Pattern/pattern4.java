ecade6ddc97dde7ff33549013ce8b718a6744244
Tejas-0570 <waydandetejas111@gmail.com>
2026-03-22 20:45:24 +0530

    Pattern 4

---
+++ Pattern/pattern4.java
@@ -0,0 +1,31 @@
+/*
+Patter 4 :
+ * * * *
+ * * *
+ * *
+ *
+ * *
+ * * *
+ * * * *
+
+ */
+
+public class pattern4 {
+    public static void main(String[] args) {
+        int n = 7;
+        int star = n/2, space = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j <= star; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+            if(i >= n/2){
+                star++;
+            } else {
+                star--;
+            }
+
+        }
+    }
+
+}
