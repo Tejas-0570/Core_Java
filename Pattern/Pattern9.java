b536e3dccedba51f5ac114cca907d99459d751a7
Tejas-0570 <waydandetejas111@gmail.com>
2026-03-22 23:43:08 +0530

    Pattern 9

---
+++ Pattern/Pattern9.java
@@ -0,0 +1,28 @@
+/*
+Pattern 9 :
+
+      *
+    *   *
+  *       *
+*           *
+  *       *
+    *   *
+      *
+
+ */
+
+public class Pattern9 {
+    public static void main(String[] args) {
+        int n = 7;
+        for(int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i+j == n/2 || i+j == n+(n/2)-1 || i-j == n/2 || j-i == n/2){
+                    System.out.print("*"+" ");
+                } else {
+                    System.out.print(" "+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
--- Pattern/pattern4.java
+++ Pattern/pattern4.java
@@ -13,7 +13,7 @@ Patter 4 :
 public class pattern4 {
     public static void main(String[] args) {
         int n = 7;
-        int star = n/2, space = 0;
+        int star = n/2;
         for(int i = 0; i < n; i++){
             for(int j = 0; j <= star; j++){
                 System.out.print("* ");
