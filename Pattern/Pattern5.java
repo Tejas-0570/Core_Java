bff4b47be78e7d4fcea73fff5bbc5f9f05922605
Tejas-0570 <waydandetejas111@gmail.com>
2026-03-22 20:53:37 +0530

    Pattern 5

---
+++ Pattern/Pattern5.java
@@ -0,0 +1,30 @@
+/*
+Patter 5 :
+*
+* *
+* * *
+* * * *
+* * *
+* *
+*
+
+ */
+
+
+public class Pattern5 {
+    public static void main(String[] args) {
+        int n = 7, star = 1;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < star; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+            if(i >= n/2){
+                star--;
+            }
+            else{
+                star++;
+            }
+        }
+    }
+}
