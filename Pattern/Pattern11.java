89b287033a3bc3d1cd237c9011f6cfaf9e6afe5c
Tejas-0570 <waydandetejas111@gmail.com>
2026-03-22 23:54:07 +0530

    Pattern 11

--- Pattern/Pattern11.java
+++ Pattern/Pattern11.java
@@ -1,9 +1,9 @@
-public class Pattern10 {
+public class Pattern11 {
     public static void main(String[] args) {
         int n = 4;
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= n; j++){
-                System.out.print(i+" ");
+                System.out.print(j+" ");
             }
             System.out.println();
         }
