f683f35cdda8dacbad839c08deb4fab97a4923d2
Tejas-0570 <waydandetejas111@gmail.com>
2026-04-07 14:17:07 +0530

    Default Values of each datatype

---
+++ Static/DefaultValuesOfDatatypes.java
@@ -0,0 +1,31 @@
+/*
+Byte : 0
+Short : 0
+Int : 0
+Float : 0.0
+Char :
+Long : 0
+Double : 0.0
+Boolean : false
+ */
+
+class DefaultValuesOfDatatypes{
+    static byte Num1 ;
+    static short Num2 ;
+    static int Num3 ;
+    static float Num4 ;
+    static char Num5 ;
+    static long Num6 ;
+    static double Num7 ;
+    static boolean Num8 ;
+    public static void main(String[] args) {
+        System.out.println("Byte : "+Num1);
+        System.out.println("Short : "+Num2);
+        System.out.println("Int : "+Num3);
+        System.out.println("Float : "+Num4);
+        System.out.println("Char : "+Num5);
+        System.out.println("Long : "+Num6);
+        System.out.println("Double : "+Num7);
+        System.out.println("Boolean : "+Num8);
+    }
+}
