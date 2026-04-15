9b5425b530af43dcabc03f27f1186c3526b747c9
Tejas-0570 <waydandetejas111@gmail.com>
2026-04-16 02:28:06 +0530

    Perfect Number using Method

--- Methods/NumberCodeUsingMethods.java
+++ Methods/NumberCodeUsingMethods.java
@@ -180,6 +180,16 @@ class NumberCodeUsingMethods{
     }
 
 
+    // Method for Perfect Number
+    public static boolean isPerfect(int num){
+        int sum = 0;
+        for(int i = 1; i <= num/2; i++){
+            if(num % i == 0) sum += i;
+        }
+        return num == sum;
+    }
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number : ");
@@ -273,6 +283,15 @@ class NumberCodeUsingMethods{
             System.out.println(palindrome+" is a Palindrome Number");
         } else {
             System.out.println(palindrome+" is not a Palindrome Number");
+        }
+
+        //------------- Method call statement for Perfect Number ---------------
+        System.out.println();
+        int perfect = 6;
+        if(isPerfect(perfect)){
+            System.out.println(perfect+" is a Perfect Number");
+        } else {
+            System.out.println(perfect+" is not a Perfect Number");
         }
     }
 }
