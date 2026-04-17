b2bffdee796a2753a2cd1ca158ffd54561a60046
Tejas-0570 <waydandetejas111@gmail.com>
2026-04-17 17:22:41 +0530

    Digit ascending and descending order

---
+++ Numbers/DigitInAscending.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class DigitInAscending {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number excluding zero : ");
+        if(!sc.hasNextLong()){
+            System.out.println("Invalid Input! Please enter valid long");
+            return;
+        }
+        long num = sc.nextLong();
+        long asce = 0;
+        for(int i = 1; i <= 9; i++){
+            long temp = num;
+            while(temp != 0){
+                if(temp % 10 == i){
+                    asce = asce * 10 + (temp % 10);
+                }
+                temp /= 10;
+            }
+        }
+
+        System.out.println(asce);
+
+
+    }
+}
---
+++ Numbers/DigitInDescending.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class DigitInDescending {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number : ");
+        if(!sc.hasNextLong()){
+            System.out.println("Invalid Input! Please enter valid long integer");
+            return;
+        }
+        long num = sc.nextLong();
+        long desc = 0;
+        for(int i = 9; i >= 1; i--){
+            long temp = num;
+            while(temp != 0){
+                if(temp % 10 == i){
+                    desc = desc * 10 + (temp % 10);
+                }
+                temp /= 10;
+            }
+        }
+        System.out.println(desc);
+    }
+}
