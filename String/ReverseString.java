2d5ca39dcebc9ac74a8514929d158e084f489864
Tejas-0570 <waydandetejas111@gmail.com>
2026-06-23 14:04:40 +0530

    Optimized Code for Reverse String

--- String/ReverseString.java
+++ String/ReverseString.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+//---------------------- Normal Answer ------------------------
 class ReverseString{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -11,3 +12,22 @@ class ReverseString{
         System.out.print("Reverse String : "+str2);
     }
 }
+/*
+
+-------------------------- Optimized Answer --------------------------
+
+class ReverseString{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        System.out.print("Enter a String : ");
+        String str = sc.next();
+        for(int i = str.length()-1; i >= 0; i--){
+            sb.append(str.charAt(i));
+        }
+        System.out.println(sb.toString());
+    }
+}
+
+
+ */
