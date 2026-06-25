033b0f70fe178ed97930c844edcf2c7f25521656
Tejas-0570 <waydandetejas111@gmail.com>
2026-06-25 12:19:15 +0530

    Improved code for Palindrome String

--- String/PalindromeString.java
+++ String/PalindromeString.java
@@ -1,3 +1,4 @@
+/*
 import java.util.Scanner;
 public class PalindromeString {
     public static void main(String[] args) {
@@ -14,6 +15,27 @@ public class PalindromeString {
             System.out.println("Palindrome");
         } else {
             System.out.println("Not Palindrome");
+        }
+    }
+}
+*/
+import java.util.Scanner;
+public class PalindromeString{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a String : ");
+        String str = sc.next().toLowerCase();
+        boolean isPalindrome = true;
+        for(int i = 0; i < str.length()/2; i++){
+            if(str.charAt(i) != str.charAt(str.length()-1-i)){
+                isPalindrome = false;
+                break;
+            }
+        }
+        if(isPalindrome){
+            System.out.println(str+" is a palindrome");
+        } else {
+            System.out.println(str+" is not a palindrome");
         }
     }
 }
