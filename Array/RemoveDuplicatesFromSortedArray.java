d264eee503b35bc64b941aaa360cc8b8518ddab7
Tejas-0570 <waydandetejas111@gmail.com>
2026-07-08 23:41:19 +0530

    Remove duplicates from sorted array

---
+++ Array/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,16 @@
+/*
+Remove duplicates from sorted array
+
+Given a sorted array, remove duplicate elements and print only unique values.
+No collections allowed.
+
+Input: {1, 1, 2, 3, 3, 4, 5, 5}
+Output: {1, 2, 3, 4, 5}
+ */
+
+
+public class RemoveDuplicatesFromSortedArray {
+    public static void main(String[] args) {
+
+    }
+}
