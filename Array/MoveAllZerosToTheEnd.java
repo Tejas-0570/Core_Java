fcf0a769de09227e583dc92ade812a595025290c
Tejas-0570 <waydandetejas111@gmail.com>
2026-07-09 22:13:24 +0530

    Move all zeros to the end

---
+++ Array/MoveAllZerosToTheEnd.java
@@ -0,0 +1,16 @@
+/*
+Move all zeros to the end
+
+Move all 0s in the array to the end while keeping the order of non-zero elements unchanged.
+Do it without a second array.
+
+Input: {0, 1, 0, 3, 12}
+Output: {1, 3, 12, 0, 0}
+ */
+
+import java.util.Scanner;
+public class MoveAllZerosToTheEnd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
