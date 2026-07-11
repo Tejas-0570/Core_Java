bd597e6c0d04a9fa0154a55125dd28f25937504a
Tejas-0570 <waydandetejas111@gmail.com>
2026-07-11 21:45:15 +0530

    Leaders in an array

---
+++ Array/LeadersInAnArray.java
@@ -0,0 +1,15 @@
+/*
+Leaders in an array
+
+An element is a "leader" if it is greater than all elements to its right.
+The rightmost element is always a leader.
+
+Input: {16, 17, 4, 3, 5, 2}
+Output: Leaders: 17, 5, 2
+ */
+
+public class LeadersInAnArray {
+    public static void main(String[] args) {
+
+    }
+}
