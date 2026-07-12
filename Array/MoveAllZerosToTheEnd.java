ea8021a22f1aeb52fe42784efe11d76a789e3285
Tejas-0570 <waydandetejas111@gmail.com>
2026-07-12 22:59:13 +0530

    Move all zeros to the end Complted

--- Array/MoveAllZerosToTheEnd.java
+++ Array/MoveAllZerosToTheEnd.java
@@ -12,7 +12,13 @@ import java.util.Scanner;
 public class MoveAllZerosToTheEnd {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = {0, 1, 0, 3, 12};
+        System.out.print("Enter size of an Array : ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for(int i = 0; i < arr.length; i++){
+            System.out.print("Enter array element at index "+i+" : ");
+            arr[i] = sc.nextInt();
+        }
         int insertPos = 0;
         for(int i = 0; i < arr.length; i++){
             if(arr[i] != 0){
