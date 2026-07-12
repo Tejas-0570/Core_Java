40c37021c73d7341b41804990f4eadcf626f2e4c
Tejas-0570 <waydandetejas111@gmail.com>
2026-07-12 19:42:47 +0530

    Leaders in an array Completed

--- Array/LeadersInAnArray.java
+++ Array/LeadersInAnArray.java
@@ -10,6 +10,19 @@ Output: Leaders: 17, 5, 2
 
 public class LeadersInAnArray {
     public static void main(String[] args) {
+        int[] arr = {16, 17, 4, 3, 5, 2};
+        int[] leaders = new int[arr.length];
+        int max = Integer.MIN_VALUE, cnt = 0;
+        for(int i = arr.length-1; i >= 0; i--){
+            if(arr[i] > max){
+                max = arr[i];
+                leaders[cnt++]= max;
+            }
+        }
 
+        System.out.print("Leaders : ");
+        for(int i = cnt-1; i >= 0; i--){
+            System.out.print(leaders[i]+" ");
+        }
     }
 }
