137b3108d22d165cf80e29238c571e6596eb590e
Tejas-0570 <waydandetejas111@gmail.com>
2026-07-12 19:51:32 +0530

    Leaders in an array, using User Input

--- Array/LeadersInAnArray.java
+++ Array/LeadersInAnArray.java
@@ -7,11 +7,18 @@ The rightmost element is always a leader.
 Input: {16, 17, 4, 3, 5, 2}
 Output: Leaders: 17, 5, 2
  */
-
+import java.util.Scanner;
 public class LeadersInAnArray {
     public static void main(String[] args) {
-        int[] arr = {16, 17, 4, 3, 5, 2};
-        int[] leaders = new int[arr.length];
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of an Array : ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for(int i = 0; i < size; i++){
+            System.out.print("Enter array element at index "+i+" : ");
+            arr[i] = sc.nextInt();
+        }
+        int[] leaders = new int[size];
         int max = Integer.MIN_VALUE, cnt = 0;
         for(int i = arr.length-1; i >= 0; i--){
             if(arr[i] > max){
