11f5f65324ee7812f71d477fd2bc9c9810a6e044
Tejas-0570 <waydandetejas111@gmail.com>
2026-07-18 18:32:16 +0530

    Maximum subarray sum (Kadane's concept)

---
+++ Array/MaximumSubarraySum.java
@@ -0,0 +1,16 @@
+/*
+Maximum subarray sum (Kadane's concept)
+
+Find the contiguous subarray that has the largest sum.
+The array can have negative numbers.
+
+Input: {-2, 1, -3, 4, -1, 2, 1, -5, 4}
+Output: Max subarray sum: 6 → {4, -1, 2, 1}
+ */
+
+import java.util.Scanner;
+public class MaximumSubarraySum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
